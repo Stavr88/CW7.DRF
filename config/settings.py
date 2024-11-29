d2b36b7551e6ee4b365cff1c82e268104d7358d7
Yura Sarvas <grey-matter88@mail.ru>
2024-11-29 13:02:48 +0300

    refactory settings.py POSTGRES_DB

--- config/settings.py
+++ config/settings.py
@@ -101,7 +101,7 @@ REST_FRAMEWORK = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("POSTGRES_NAME"),
+        "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
